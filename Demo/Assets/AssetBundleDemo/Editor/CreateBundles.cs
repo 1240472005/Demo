4c25517a8966650151afcc0216aa16c1190e70e5
qccoder <1240472005@qq.com>
2021-02-08 18:53:31 +0800

    FirstCommit

---
+++ Demo/Assets/AssetBundleDemo/Editor/CreateBundles.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class CreateBundles 
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string outPath = "Assets/AssetBundleDemo/AssetBundles";
+        if (!System.IO.Directory.Exists(outPath))
+        {
+            System.IO.Directory.CreateDirectory(outPath); 
+        }
+        BuildPipeline.BuildAssetBundles(outPath, BuildAssetBundleOptions.None,BuildTarget.StandaloneWindows);
+    }
+}
