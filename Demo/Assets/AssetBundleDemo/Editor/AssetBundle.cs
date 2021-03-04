05d4938479d72defd4e7d31be9c82d130c8644f2
qccoder <1240472005@qq.com>
2021-03-04 19:08:11 +0800

    2

---
+++ Demo/Assets/AssetBundleDemo/Editor/AssetBundle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class AssetBundle 
+{
+    [MenuItem("AssetBundle/SetAssetBundleLabels")]
+    public static void SetAssetBundleLables()
+    {
+        //移除所有没有使用标记的标记
+        AssetDatabase.RemoveUnusedAssetBundleNames();
+        //路径
+        string path = Application.streamingAssetsPath;
+        DirectoryInfo directoryInfo = new DirectoryInfo(path);
+        DirectoryInfo[] directoryInfos = directoryInfo.GetDirectories();
+        foreach (var v in directoryInfos)
+        {
+            
+        }
+    }
+}
