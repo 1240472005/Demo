2a4b7ea34467d4b9515d9f8111a468562546936b
qccoder <1240472005@qq.com>
2021-03-09 19:11:02 +0800

    加载

---
+++ Demo/Assets/AssetBundleDemo/Script/AssetBundleLoader.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+/// <summary>
+/// 加载Assetbundle;
+/// </summary>
+public class AssetBundleLoader 
+{
+    private AssetLoader assetLoader;
+    /// <summary>
+    /// 通过WWW 加载
+    /// </summary>
+    private WWW www;
+    /// <summary>
+    /// 包名
+    /// </summary>
+    private string bundleName;
+    /// <summary>
+    /// 包的路径
+    /// </summary>
+    private string bundlePath;
+    /// <summary>
+    /// 进度
+    /// </summary>
+    private float progress;
+
+    private Action<string, float> loadProgress;
+    /// <summary>
+    /// 加载完成
+    /// </summary>
+    private Action<string> loadComplete;
+    /// <summary>
+    /// 加载资源包
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator Load()
+    {
+        progress = 0.0f ;
+        www = new WWW(bundlePath);
+        while (!www.isDone)
+        {
+            progress = www.progress;
+            if (loadProgress != null)
+                loadProgress(bundleName, progress);
+            yield return www;
+        }
+        progress = www.progress;
+        if (progress >= 1.0f)
+        {
+            assetLoader = new AssetLoader();
+            assetLoader.SetAssetBundle(www.assetBundle);
+            if (loadProgress != null)
+                loadProgress(bundleName, progress);
+
+            if (loadComplete != null)
+            {
+                loadComplete(bundleName);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 获取单个资源
+    /// </summary>
+    /// <typeparam name="T">资源类型</typeparam>
+    /// <param name="assetName">资源名字</param>
+    /// <returns>特定的资源</returns>
+    public T LoadAsset<T>(string assetName) where T : UnityEngine.Object
+    {
+        if (assetLoader != null)
+        {
+            return  assetLoader.LoadAsset<T>(assetName);
+        }
+        return null;
+       
+    }
+    /// <summary>
+    /// 获取所有资源
+    /// </summary>
+    /// <returns></returns>
+    public UnityEngine.Object[] LoadAllAsset()
+    {
+        if (assetLoader == null)
+        {
+            
+            return null;
+        }
+        else
+        {
+            return assetLoader.LoadAllAsset();
+        }
+    }
+    /// <summary>
+    /// 获取所有资源
+    /// </summary>
+    /// <param name="assetName">资源名字</param>
+    /// <returns></returns>
+    public UnityEngine.Object[] LoadAssetWithSubAssets(string assetName)
+    {
+        if (assetLoader != null)
+        {
+             return assetLoader.LoadAssetWithSubAssets(assetName);
+        }
+        else
+        {
+            return null;
+        }
+    }
+    /// <summary>
+    /// 卸载资源
+    /// </summary>
+    /// <param name="asset"></param>
+    public void UnLoadAsset(UnityEngine.Object asset)
+    {
+        if (this.assetLoader == null) return;
+        assetLoader.UnLoadAsset(asset);
+    }
+    /// <summary>
+    /// 释放资源
+    /// </summary>
+    public void Dispose()
+    {
+        if (assetLoader != null)
+        {
+            assetLoader.Dispose();
+        }
+        assetLoader = null;
+    }
+}
---
+++ Demo/Assets/AssetBundleDemo/Script/AssetLoader.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetLoader : System.IDisposable
+{
+    private AssetBundle m_assetBundle;
+
+
+    /// <summary>
+    /// 设置资源包
+    /// </summary>
+    /// <param name="bundle"></param>
+    public void SetAssetBundle(AssetBundle bundle)
+    {
+        this.m_assetBundle = bundle;
+    }
+    /// <summary>
+    /// 获取单个资源
+    /// </summary>
+    /// <typeparam name="T">资源类型</typeparam>
+    /// <param name="assetName">资源名字</param>
+    /// <returns>特定的资源</returns>
+    public T LoadAsset<T>(string assetName) where T : Object
+    {
+        if (m_assetBundle != null)
+        {
+            if (m_assetBundle.Contains(assetName))
+            {
+                return m_assetBundle.LoadAsset(assetName) as T;
+            }
+            else
+            {
+                Debug.LogError(($"当前资源包不包含{0}资源", assetName));
+                return null;
+            }
+        }
+        else
+        {
+            Debug.LogError(($"当前资源包为空，无法获取{0}资源", assetName));
+            return null;
+        }
+    }
+    /// <summary>
+    /// 获取所有资源
+    /// </summary>
+    /// <returns></returns>
+    public Object[] LoadAllAsset()
+    {
+        if (m_assetBundle == null)
+        {
+            Debug.LogError(($"当前资源包{0}为空", m_assetBundle.name));
+            return null;
+        }
+        else
+        {
+            return m_assetBundle.LoadAllAssets();
+        }
+    }
+    /// <summary>
+    /// 获取所有资源
+    /// </summary>
+    /// <param name="assetName">资源名字</param>
+    /// <returns></returns>
+    public Object[] LoadAssetWithSubAssets(string assetName)
+    {
+        if (m_assetBundle != null)
+        {
+            if (m_assetBundle.Contains(assetName))
+            {
+                return m_assetBundle.LoadAssetWithSubAssets(assetName);
+            }
+            else
+            {
+                Debug.LogError(($"当前资源包不包含{0}资源", assetName));
+                return null;
+            }
+        }
+        else
+        {
+            Debug.LogError(($"当前资源包为空，无法获取{0}资源", assetName));
+            return null;
+        }
+    }
+    /// <summary>
+    /// 卸载资源
+    /// </summary>
+    /// <param name="asset"></param>
+    public void UnLoadAsset(Object asset)
+    {
+        if (this.m_assetBundle == null) return;
+        Resources.UnloadAsset(asset);
+    }
+    /// <summary>
+    /// 释放资源
+    /// </summary>
+    public void Dispose()
+    {
+        if (m_assetBundle != null)
+        {
+            
+            m_assetBundle.Unload(false);
+        }
+    }
+
+    public void GetAll()
+    {
+        string[] names = m_assetBundle.GetAllAssetNames();
+        foreach (var item in names)
+        {
+            Debug.Log(item);
+        }
+    }
+
+    
+}
